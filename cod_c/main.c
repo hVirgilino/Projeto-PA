c808c5dd531f2118cfb9e2fb522c611c47b856d2
Henrique Virgilino <henriquevirgilino@ucl.br>
2022-06-19 02:50:33 -0300

    Implementação dos menus

--- cod_c/main.c
+++ cod_c/main.c
@@ -3,47 +3,142 @@
 */
 
 #include<stdio.h>
+#include<string.h>
 #include<winsock2.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
 #define clrscr() printf("\e[1;1H\e[2J")
 
-int recv_size, etc, etc2, escolha, escolhaLE, escolhaLES, escolhaAT,leituras;
-char *message , server_reply[2000], string[4];
+int recv_size, etc, tamResposta, escolha, escolhaLE, escolhaLES, escolhaAT,leituras;
+char *message , server_reply[2000], string[4], qtdLeitura[1], qtdAtuacao[3];
+SOCKET s;
 
-void menuPrincipal(){
-    
+void Sair(){
+    clrscr();
+    message = "EXIT#";
+    send(s , message , strlen(message) , 0);
+    return;
+}
+
+void MenuPrincipal(){
+    clrscr();
     printf("\n\nO que fazer?\n");
     printf("1 - Ler dados do sensor\n");
     printf("2 - Enviar comandos para o atuador\n");
+    printf("3 - Sair\n");
     scanf("%d", &escolha);
 
-    if (escolha = 1){
-        printf("1 - Ler dados do sensor 1x\n");
-        printf("2 - Ler dados do sensor 3x\n");
-        scanf("%d", &leituras);
+    switch (escolha)
+    {
+    case 1:
+        MenuEscolhaLeitura();
+        break;
+    case 2:
+        MenuAtuador();
+        break;
+    case 3:
+        Sair();
+        break;
+    
+    default:
+        break;
+    }
+}
 
-        if (leituras = 1){
-            //formular onde le 1x
+void MenuEscolhaLeitura(){
+    clrscr();
+    message = "L";
+    printf("\n\nO que fazer?\n");
+    printf("1 - Ler sensor\n");
+    printf("2 - Ler atuador\n");
+    printf("3 - Voltar\n");
+    printf("4 - Sair\n");
+    scanf("%d", &escolha);
+
+    switch (escolha)
+    {
+    case 1:
+        strcat(message, "0");
+        MenuLeitura();
+        break;
+    case 2:
+        strcat(message, "1");
+        MenuLeitura();
+        break;
+    case 3:
+        MenuPrincipal();
+        break;
+    case 4:
+        Sair();
+        break;
+    
+    default:
+        break;
+    }
+}
+
+void MenuLeitura(){
+    
+    clrscr();
+    printf("\n\nO que fazer?\n");
+    printf("1 - Leitura única\n");
+    printf("2 - Leitura em série\n");
+    printf("3 - Voltar\n");
+    printf("4 - Sair\n");
+    scanf("%d", &escolha);
+
+    switch (escolha)
+    {
+    case 1:
+        strcat(message, "001");
+        break;
+    case 2:
+        clrscr();
+        printf("\nInsira \"V\" para retornar ao menu anterior ou \"S\" para encerrar a conexão\nInsira abaixo a quantidade leituras em série desejada: (2 a 4)\n");
+        scanf("%d", &qtdLeitura);
+
+        if(qtdLeitura == "V"){
+            MenuLeitura();
+        }else if(qtdLeitura == "S"){
+            Sair();
         }
-        if (leituras = 2){
-            //formular onde le 3x
-        }
+
+        strcat(message, "00");
+        strcat(message, qtdLeitura);
+        break;
+    case 3:
+        MenuEscolhaLeitura();
+        break;
+    case 4:
+        Sair();
+        break;
+    
+    default:
+        break;
     }
 
-    if (escolha = 2){
-        printf("1 - Ler dados do atuador 1x\n");
-        printf("2 - Ler dados do atuador 3x\n");
-        scanf("%d", &leituras);
-        if (leituras = 1){
-            //formular onde le 1x
-        }
-        if (leituras = 2){
-            //formular onde le 3x
-        }
+    strcat(message, "#");
+
+    send(s , message , strlen(message) , 0);
+}
+
+void MenuAtuador(){
+    message = "A0";
+    printf("\nInsira \"V\" para retornar ao menu anterior ou \"S\" para encerrar a conexão\nInsira abaixo o valor correspondente à porcentagem de atuação desejada\n");
+    scanf("%d", &qtdAtuacao);
+
+    if(qtdAtuacao[0] == "V"){
+        MenuLeitura();
+    }else if(qtdAtuacao[0] == "S"){
+        Sair();
     }
 
+    strcat(message, qtdAtuacao);
+
+    strcat(message, "#");
+
+    send(s , message , strlen(message) , 0);
 }
 
 int main(int argc , char *argv[])
@@ -54,19 +149,15 @@ int main(int argc , char *argv[])
 	setvbuf(stderr, NULL, _IONBF, 0);
 
     WSADATA wsa;
-    SOCKET s;
+    
     
     struct sockaddr_in server;
 
-    
-    
-    printf("\nInitialising Winsock...");
+
     if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
     {
         printf("Failed. Error Code : %d",WSAGetLastError());
     }
-
-    printf("Initialised.\n");
 
     //Create a socket
     if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
@@ -74,8 +165,6 @@ int main(int argc , char *argv[])
         printf("Could not create socket : %d" , WSAGetLastError());
         return 1;
     }
-
-    printf("Socket created.\n");
 
     server.sin_addr.s_addr = inet_addr("192.168.0.116");
     server.sin_family = AF_INET;
@@ -87,104 +176,26 @@ int main(int argc , char *argv[])
         puts("connect error");
         return 1;
     }
+    message = "G#";
+    send(s , message , strlen(message) , 0);
+    tamResposta = recv(s , server_reply , 2000 , 0);
 
-    puts("Connected");
+    server_reply[tamResposta] = '\0';
+    puts(server_reply);
     /*#############################################################################*/
 
-    menuPrincipal();
-
-    /*switch (escolha){
-        case 1:
-            clrscr();
-            printf("\n\nO que fazer?\n");
-            printf("1 - Ler dados\n");
-            printf("2 - Ler dados em serie (max 3)\n");
-            scanf("%d", &escolhaLE);
-
-            switch (escolhaLE)
-            {
-            case 1:
-                
-                //Send some data
-                message = "GET / HTTP/1.1\r\n\r\n";
-
-                if((etc = send(s , message , strlen(message) , 0)) < 0)
-                {
-                    puts("Send failed");
-                }
-                printf("Data Sent: %d bytes\n", etc);
-
-                etc2 = recv(s , server_reply , 2000 , 0);
-
-                //Add a NULL terminating character to make it a proper string before printing
-                server_reply[etc2] = '\0';
-                puts(server_reply);
-
-                break;
-            
-            case 2:
-                
-                printf("Digite a quantidade de leituras em serie a serem feitas:\n");
-                scanf("%d", &escolhaLES);
-
-                if(escolhaLES < 1){
-                    escolhaLES = 1;
-                }
-                
-                if(escolhaLES > 8){
-                    escolhaLES = 8;
-                }
-
-                message = "GET / HTTP/1.1\r\n\r\n";
-
-                if((etc = send(s , message , strlen(message) , 0)) < 0)
-                {
-                    puts("Send failed");
-                }
-                printf("Data Sent: %d bytes\n", etc);
-
-                for(int c = 0; c < escolhaLES; c++)
-                {
-                    etc = recv(s , server_reply , 2000 , 0);
-
-                    //Add a NULL terminating character to make it a proper string before printing
-                    server_reply[etc] = '\0';
-                    puts(server_reply);
-                }
-
-                break;
-            
-            default:
-                puts("Digite o numero das alternativas que corresponde a acao que deseja realizar.");
-                return 1;
-                break;
-            }
-
+    do
+    {
+        if(message == "EXIT#"){
             break;
+        }else{
+            MenuPrincipal();
+        }
+        
+    } while (message != "EXIT#");
+    
 
-        case 2:
-            clrscr();
-            printf("\n\nInsira a porcentagem");
-
-            scanf("%d", &escolhaAT);
-
-            
-
-            
-
-            if((etc = send(s , message , strlen(message) , 0)) < 0)
-            {
-                puts("Send failed");
-            }
-            printf("Data Sent: %d bytes\n", etc);
-
-            break;
-
-        default:
-            puts("Digite o numero das alternativas que corresponde a acao que deseja realizar.");
-            return 1;
-            break;
-    } */
+    
     
     return 0;
 }
